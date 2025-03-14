466317e99c30d75b8154092c428f13a1b1d9cb97
Yanchelo <yanmelnychenko06@gmail.com>
2025-03-14 16:14:10 +0100

    änderung

--- src/Important.jsx
+++ src/Important.jsx
@@ -6,5 +6,4 @@ export default function ImportentComponent()
     title = {'Wichtig'}
     img src = 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.thenounproject.com%2Fpng%2F551749-200.png&tbnid=_59UUeeYhh3LSM&vet=10CAoQxiAoAWoXChMI2JjRxIDziwMVAAAAAB0AAAAAEAc..i&imgrefurl=https%3A%2F%2Fthenounproject.com%2Fbrowse%2Ficons%2Fterm%2Fdrop-down-arrow%2F&docid=RHfybWygxx8cpM&w=200&h=200&itg=1&q=foto%20arrow%20down&ved=0CAoQxiAoAWoXChMI2JjRxIDziwMVAAAAAB0AAAAAEAc' alt='arrow down'
 />
-
 }
