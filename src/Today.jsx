a3facd3bb7c425414dca409d2155cb8e688dc003
Mm-lay4 <marten.meissner@layer-four.de>
2025-03-23 21:24:30 +0100

    solve merge conflicts

--- src/Today.jsx
+++ src/Today.jsx
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 import {todos} from './todo.json';
-import ToDoHeader from './TodoHeader';
+import ToDoHeader from './components/TodoHeader';
 
 export default function TodayComponent({todos}){
 
@@ -9,16 +8,4 @@ export default function TodayComponent({todos}){
         title={'Heute'}
         // height={200}
     />
-=======
-import {todos} from './todo.json';
-import ToDoHeader from './TodoHeader';
-
-export default function TodayComponent({todos}){
-
-
-  return  <ToDoHeader
-        title={'Heute'}
-        // height={200}
-    />
->>>>>>> b9f8d361d302f0307c0730ed24accc727e76182a
 }
